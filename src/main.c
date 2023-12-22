1a6a9717d185e5d55bb79e2db0609e511c17d100
pilatbpw <01187576@pw.edu.pl>
2023-12-22 17:45:52 +0100

    Dodano wczytywanie argumentow przy uzyciu getopt. Dodano wykrywanie bledow przy wprowadzaniu argumentow. Dodano plik Makefile.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(){
+int main(int argc, char *argv[]) {
+    int opcje;
+    int m = 0;  // Liczba wierszy
+    int n = 0;  // Liczba kolumn
+    int iteracje = 0;  // Liczba iteracji
+    char *nazwa_wynikowa = NULL;  // Nazwa plików wynikowych
+    char *kierunek = NULL;  // Kierunek początkowy mrowki g - gora d -dol p - prawo l - lewo
 
+    while ((opcje = getopt(argc, argv, "m:n:i:p:k:")) != -1) {
+        switch (opcje) {
+            case 'm':
+				if(m!=0){
+					fprintf(stderr, "BLAD: Wielokrotne uzycie tego samego argumentu\n");
+					exit(1);
+				}
+				if(optarg[0]=='-'){
+					fprintf(stderr, "Nie podano poprawnej wartosci dla argumentu -%c\n", 'm');
+					exit(1);
+				}
+                m = atoi(optarg);
+                break;
+            case 'n':
+				if(n!=0){
+					fprintf(stderr, "BLAD: Wielokrotne uzycie tego samego argumentu\n");
+					exit(1);
+				}
+				if(optarg[0]=='-'){
+					fprintf(stderr, "Nie podano poprawnej wartosci dla argumentu -%c\n", 'n');
+					exit(1);
+				}
+                n = atoi(optarg);
+                break;
+            case 'i':
+				if(iteracje!=0){
+					fprintf(stderr, "BLAD: Wielokrotne uzycie tego samego argumentu\n");
+					exit(1);
+				}
+				if(optarg[0]=='-'){
+					fprintf(stderr, "Nie podano poprawnej wartosci dla argumentu -%c\n", 'i');
+					exit(1);
+				}
+                iteracje = atoi(optarg);
+                break;
+            case 'p':
+				if(nazwa_wynikowa!=NULL){
+					fprintf(stderr, "BLAD: Wielokrotne uzycie tego samego argumentu\n");
+					exit(1);
+				}
+				if(optarg[0]=='-'){
+					fprintf(stderr, "Nie podano poprawnej wartosci dla argumentu -%c\n", 'p');
+					exit(1);
+				}
+                nazwa_wynikowa = optarg;
+                break;
+            case 'k':
+				if(kierunek!=NULL){
+					fprintf(stderr, "BLAD: Wielokrotne uzycie tego samego argumentu\n");
+					exit(1);
+				}
+				if(optarg[0]=='-'){
+					fprintf(stderr, "Nie podano poprawnej wartosci dla argumentu -%c\n", 'k');
+					exit(1);
+				}
+                kierunek = optarg;
+                break;
+			
+            case '?':
+				if(optopt == 'm' ||optopt == 'n' ||optopt == 'i' ||optopt == 'p' ||optopt == 'k'){
+					fprintf(stderr, "Nie podano wartosci dla argumentu -%c\n", optopt);
+					exit(1);
+					
+				}
+                fprintf(stderr, "Nieznana opcja %c\n", optopt);
+				
+                break;
+            default:
+                fprintf(stderr, "Niepoprawna opcja: %c\n", optopt);
+                break;
+        }
+		
+        if (opcje == '?') {
+            break;
+        }
+    }
 
+    printf("Opcje m=%i n=%i iteracje=%i nazwa_wynikowa=%s kierunek=%s\n", m, n, iteracje, nazwa_wynikowa, kierunek);
+    return 0;
 }
